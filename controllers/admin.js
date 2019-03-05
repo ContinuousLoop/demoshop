734295ae1aee4a6e5bed8088237171d7d66be63b
ContinuousLoop <v.cho@hotmail.ca>
2019-03-05 15:05:53 -0800

    edit product function

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,40 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product"
+    path: "/admin/add-product",
+    editing: false
   });
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit
+  const productId = req.params.productId
+  if(!editMode){
+    res.redirect('/');
+  }
+  Product.findProductById(productId, product => {
+    res.render("admin/edit-product",{   
+          pageTitle: "Edit Product",
+          path: "/admin/edit-product",
+          editing: editMode,
+          product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  const title = req.body.title;
+  const imgUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(productId, title, imgUrl, price, description)
+  product.save();
+  res.redirect("/");
+
+}
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
--- models/product.js
+++ models/product.js
@@ -14,32 +14,45 @@ const getProducts = callback => {
 }
 
 module.exports = class Product {
-    constructor(_title, _imgUrl, _price, _description) {
+    constructor(_id, _title, _imgUrl, _price, _description) {
+        this.id = _id;
         this.title = _title;
         this.imgUrl = _imgUrl;
         this.price = _price;
         this.description = _description;
     }
 
+    // Retrieve products and check if the id === productId if product id already
+    // exists, find index and replace it with new item write to file
+
     save() {
-        this.id = Math.random().toString();
         getProducts(products => {
-            products.push(this);
-            fs.writeFile(_path, JSON.stringify(products), (err) => {
-                console.log(err);
+            if (this.id) {
+                let productIndex = products.findIndex(product => product.id === this.id);
+                let newProducts = [...products];
+                newProducts[productIndex] = this;
+                fs.writeFile(_path, JSON.stringify(newProducts), error => {
+                    console.log(error);
+                })
+            } else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(_path, JSON.stringify(products), (err) => {
+                    console.log(err);
+                })
+            }
+
+        })
+    }
+
+        static fetchAll(callback) {
+            getProducts(callback);
+        }
+
+        static findProductById(productId, cb) {
+            getProducts(products => {
+                let product = products.find(product => product.id === productId);
+                cb(product);
             })
-        })
-
+        }
     }
-
-    static fetchAll(callback) {
-        getProducts(callback);
-    }
-
-    static findProductById(productId, cb){
-        getProducts(products => {
-            let product = products.find( product => product.id === productId );
-            cb(product);
-        })
-    }
-}
